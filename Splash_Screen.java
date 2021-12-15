d64dc5f1b399376d97fe87cd6acf4873be35011b
sayambothra <63236442+sayambothra@users.noreply.github.com>
2021-12-15 10:47:03 +0530

    Add files via upload

---
+++ Splash_Screen.java
@@ -0,0 +1,24 @@
+package com.abdul.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(intent);
+                
+            }
+        },500000);
+    }
+}
