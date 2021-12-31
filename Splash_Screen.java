74cd040527ea5988d73aaa49504ba9f6d0529f69
sayambothra <63236442+sayambothra@users.noreply.github.com>
2021-12-31 15:18:19 +0530

    Update Splash_Screen.java

--- Splash_Screen.java
+++ Splash_Screen.java
@@ -1,24 +1,26 @@
 package com.abdul.myapplication;
 
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Splash_Screen extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(Splash_Screen.this,MainActivity.class);
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
-                
+
             }
-        },500000);
+        },5000);
     }
 }
